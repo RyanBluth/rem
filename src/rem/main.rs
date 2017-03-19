106c31f3f26e95b8cfe6b3e8c16d449686f42c0f
ryan <ryanbluth93@gmail.com>
2017-03-18 22:07:48 -0400

    Remove extern crates

---
+++ src/rem/main.rs
@@ -0,0 +1,87 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate backtrace;
+extern crate futures;
+extern crate futures_cpupool;
+extern crate tokio_core;
+extern crate tokio_proto;
+extern crate tokio_service;
+extern crate tokio_io;
+extern crate bytes;
+
+mod rem;
+
+use std::string::String;
+use std::env;
+use std::env::Args;
+
+use rem::error::*;
+
+/// The different run modes for REM
+enum Mode {
+    NONE,
+    CLIENT,
+    SERVER,
+}
+
+fn main() {
+    env_logger::init().unwrap();
+
+    let mut args: Args = env::args();
+
+    // Set default values for arguments
+    let mut ip: String = String::from("127.0.0.1");
+    let mut port: String = String::from("8080");
+
+    let mut mode: Mode = Mode::NONE;
+
+    // Consume the first argument since it is just the program
+    args.next();
+
+    loop {
+        match args.next().as_ref() {
+            Some(opt) => {
+                match opt.as_ref() {
+                    "server" => {
+                        mode = Mode::SERVER;
+                    }
+                    "client" => {
+                        mode = Mode::CLIENT;
+                    }
+                    "-port" => {
+                        match args.next() {
+                            Some(x) => port = x,
+                            None => break,
+                        }
+                    }
+                    "-ip" => {
+                        match args.next() {
+                            Some(x) => ip = x,
+                            None => break,
+                        }
+                    }
+                    _ => {
+                        RemError::with_reason_str_and_details(REM_00002,
+                                                              format!("Argument {} is not a \
+                                                                       valid option",
+                                                                      opt))
+                            .log_and_exit();
+                    }
+                }
+            }
+            None => break,
+        }
+    }
+
+
+    match mode {
+        Mode::CLIENT => rem::client::launch(ip, port),
+        Mode::SERVER => rem::server::launch(ip, port),
+        Mode::NONE => {
+            RemError::with_reason_str(REM_00001).log();
+        }
+    }
+
+    
+}
--- src/rem/mod.rs
+++ src/rem/mod.rs
@@ -1,12 +1,3 @@
-extern crate std;
-extern crate futures;
-extern crate futures_cpupool;
-extern crate tokio_core;
-extern crate tokio_proto;
-extern crate tokio_service;
-extern crate tokio_io;
-extern crate bytes;
-
 pub mod server;
 pub mod client;
 pub mod error;
